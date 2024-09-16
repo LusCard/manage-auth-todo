64e663069786d39d323fa40623114b86e88f7c9d
LusCard <lucascardenas@outlook.es>
2024-09-16 11:57:56 -0300

    feat: hecho los controladores para modificar temporalmente el array de todos(CREAR, EDITAR Y BORRAR)

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -5,7 +5,6 @@ export const getAllTodosCtrl = (req, res) => {
     return res.status(401).json({ message: "No autorizado" });
   }
 
-  // Filtrar los todos del usuario autenticado
   console.log(req.user);
   const userTodos = database.todos.filter((todo) => todo.owner === req.user.id);
 
@@ -13,25 +12,72 @@ export const getAllTodosCtrl = (req, res) => {
 };
 
 export const addTodo = (req, res) => {
-  const { title, completed } = req.body;
-
-  if (!title || completed === undefined) {
-    return res
-      .status(400)
-      .json({ message: "Missing required fields: title or completed" });
+  if (!req.user) {
+    return res.status(401).json({ message: "No autorizado" });
   }
 
-  const newId = database.todos.length
-    ? database.todos[database.todos.length - 1].id + 1
-    : 1;
+  const { title, completed } = req.body;
 
+  if (!title || typeof completed !== "boolean") {
+    return res.status(400).json({ message: "Datos incompletos o incorrectos" });
+  }
   const newTodo = {
-    id: newId,
-    title,
-    completed,
+    id: database.todos.length + 1,
+    title: title,
+    completed: completed,
+    owner: req.user.id,
   };
 
   database.todos.push(newTodo);
 
-  return res.status(201).json(newTodo);
+  res.status(201).json({ message: "Todo creado exitosamente", todo: newTodo });
+};
+
+export const updateTodo = (req, res) => {
+  const { todosID } = req.params;
+  const { title, completed } = req.body;
+
+  const todo = database.todos.find((todo) => todo.id === parseInt(todosID));
+
+  if (!todo) {
+    return res.status(404).json({ message: "Todo no encontrado" });
+  }
+
+  const userTodos = database.todos.filter((todo) => todo.owner === req.user.id);
+  if (!userTodos) {
+    return res
+      .status(403)
+      .json({ message: "No tienes permisos para editar este todo" });
+  }
+  if (title) {
+    todo.title = title;
+  }
+
+  if (typeof completed === "boolean") {
+    todo.completed = completed;
+  }
+
+  res.json({ message: "Todo actualizado correctamente", todo });
+};
+
+export const deleteTodo = (req, res) => {
+  const { id } = req.params;
+
+  const todoIndex = database.todos.findIndex(
+    (todo) => todo.id === parseInt(id)
+  );
+
+  if (todoIndex === -1) {
+    return res.status(404).json({ message: "Todo no encontrado" });
+  }
+
+  if (database.todos[todoIndex].owner !== req.user.id) {
+    return res
+      .status(403)
+      .json({ message: "No tienes permiso para eliminar este todo" });
+  }
+
+  const deletedTodo = database.todos.splice(todoIndex, 1);
+
+  res.json({ message: "Todo eliminado correctamente", todo: deletedTodo });
 };
